66dd027ead2851e1a6c603459420fff79516aaad
ashokramadasu <ramadasuashok@gmail.com>
2017-12-03 01:58:33 +0530

    heroku port issue

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ function sortByCount (wordCount, N) {
 
 
 //Express Server running at port 3000;
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("server running at 3000");
 });
 
