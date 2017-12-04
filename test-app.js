5ddf30d62aeb75ed3879686db6d639048301c1b3
ashokramadasu <ramadasuashok@gmail.com>
2017-12-05 00:30:41 +0530

    Update test-app.js

--- test-app.js
+++ test-app.js
@@ -1,3 +1,6 @@
+'use strict';
+
+//Npm testing Modules
 var  chai     = require('chai')
    , chaiHttp = require('chai-http')
    , server   = require('./app')
@@ -5,21 +8,73 @@ var  chai     = require('chai')
 
 chai.use(chaiHttp);
 
-describe('/test POST', () => {
-it('should give a JSON array on /test POST', function(done) {
-  var reqBody = {'Number': 100} ;
+// no of records in output is 367 . I am  testing for 100, 10000, -100, ashok values
+
+// This test for 100 which is less than 367
+it('For 100 it should give correct result on /test POST', function(done) {
+  let reqBody = {'Number': 100} ;
   chai.request(server)
     .post('/test')
     .send(reqBody)
-    .end(function(err, res, body){
+    .end(function(err, res){
       res.should.have.status(200);
       res.should.be.json;
-      body.should.be.a('array');
-      body[0].should.be.a('object');
-      body[0].should.have.property('word');
-      body[0].should.have.property('count');
+      res.body.should.be.a('array');
+      res.body[0].should.be.a('object');
+      res.body[0].should.have.property('word');
+      res.body[0].should.have.property('count');
       done();
     });
+
 });
+
+// This test for 10000 which is greater than 367. It is warning case
+it('For 10000 it shouldnot give correct result on /test POST', function(done) {
+  let reqBody = {'Number': 10000} ;
+  chai.request(server)
+    .post('/test')
+    .send(reqBody)
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('string');
+      res.body.should.equal('The result array contains 367 records . Please enter a Number between 1 to 367')
+      res.body[0].should.not.be.a('object');
+      res.body[0].should.not.have.property('word');
+      res.body[0].should.not.have.property('count');
+      done();
+    });
+
+});
+
+// This test for -100 which is negative number . It will give empty array
+it('For -100 it should give empt array on /test POST', function(done) {
+  let reqBody = {'Number': -100 } ;
+  chai.request(server)
+    .post('/test')
+    .send(reqBody)
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.should.have.lengthOf(0);
+      done();
+    });
+
+});
+
+// This test for ashok which is String value . It will give empty array
+it('For ashok it should give empty array on on /test POST', function(done) {
+  let reqBody = {'Number': 'ashok'} ;
+  chai.request(server)
+    .post('/test')
+    .send(reqBody)
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.should.have.lengthOf(0);
+      done();
+    });
 
 });
